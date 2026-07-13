5f0ff47372f7c3095daf2d3010f7e9facae3e120
Arpita <arpitagupta1203@gmail.com>
2026-07-13 22:25:05 +0530

    Added ASCII value programs

---
+++ 11. numbers to ASCII value.cpp
@@ -0,0 +1,10 @@
+//Print the ASCII characters from 65 to 90
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 65; i<=90;i++){
+        cout<<char(i)<<endl;
+    }
+    return 0;
+}
