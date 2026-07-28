4c154ee8fbb7e56e9a1903542ff66ae038e8df16
Arpita <arpitagupta1203@gmail.com>
2026-07-28 23:56:28 +0530

    Added GCD of two numbers program

---
+++ 52.Find GCD of two numbers.cpp
@@ -0,0 +1,31 @@
+//Find GCD of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,smallest;
+    int gcd=1;
+
+    cout<<"enter a number: "<<endl;
+    cin>>a;
+    cout<<"Enter a number: "<<endl;
+    cin>>b;
+
+    if(a>b){
+        smallest=a;
+    }
+    else{
+        smallest=b;
+    }
+
+    for(int i=1;i<=smallest;i++){
+        if(a%i==0 && b%i==0){
+            gcd=i;
+        }
+
+        cout<<gcd;
+    }
+    return 0;
+
+}
